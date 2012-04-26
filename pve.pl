7de331a7f39c28b3506d55da4825a21183d70d32
Brendan Beveridge <brendan@nodeintegration.com.au>
2012-04-26 11:31:01 +1000

    -add some more functionality -handle options - most of the functionality of action is complete

--- pve.pl
+++ pve.pl
@@ -5,16 +5,42 @@ use warnings;
 
 use lib './lib';
 use Net::Proxmox::VE;
+use Data::Dumper;
+use Getopt::Long;
 
+my $host     = 'host';
+my $username = 'root';
+my $password = 'password';
+my $debug    = undef;
 
-my $pve = Net::Proxmox::VE->new(
-    username => 'root',
-    password => 'password',
-    host     => 'hostname',
+GetOptions (
+    'host=s'     => \$host,
+    'username=s' => \$username,
+    'password=s' => \$password,
+    'debug'      => \$debug,
 );
 
-print "login_ok\n"  if $pve->login;
-print "ticket_ok\n" if $pve->check_login_ticket;
+my $pve = Net::Proxmox::VE->new(
+    host     => $host,
+    username => $username,
+    password => $password,
+    debug    => $debug,
+);
+
+die "login failed\n"         unless $pve->login;
+die "invalid login ticket\n" unless $pve->check_login_ticket;
 
 
-$pve->action(path => '/nodes', method => 'GET');
+# list nodes in cluster
+print Dumper $pve->action(path => '/nodes',        method => 'GET');
+
+# list nodes in cluster
+print Dumper $pve->action(path => '/access/users', method => 'GET');
+
+# Create a test user
+print Dumper $pve->action(
+    path      => '/access/users',
+    method    => 'POST',
+    post_data => {'userid' => 'testuser@foobar'}
+);
+print Dumper $pve->action(path => '/access/users/testuser@foobar', method => 'DELETE');
