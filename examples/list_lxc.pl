650105648d557c8bc7a4871ea0221b527df334b9
Dean Hamstead <dean@fragfest.com.au>
2025-03-04 22:48:37 -0800

    Add lxc example

---
+++ examples/list_lxc.pl
@@ -0,0 +1,73 @@
+#!/usr/bin/perl
+
+########################################################
+#
+#    List all lxc virtual machines on the cluster
+#
+########################################################
+
+use strict;
+use warnings;
+
+use lib './lib';
+use Net::Proxmox::VE;
+use IO::Socket::SSL qw(SSL_VERIFY_NONE);
+use Data::Dumper;
+use Getopt::Long;
+
+my $host     = 'host';
+my $username = 'user';
+my $password = 'pass';
+my $debug    = undef;
+my $realm    = 'pve'; # 'pve' or 'pam'
+
+GetOptions (
+    'host=s'     => \$host,
+    'username=s' => \$username,
+    'password=s' => \$password,
+    'debug'      => \$debug,
+    'realm=s'    => \$realm,
+);
+
+my $pve = Net::Proxmox::VE->new(
+    host     => $host,
+    username => $username,
+    password => $password,
+    debug    => $debug,
+    realm    => $realm,
+    ssl_opts => {
+        SSL_verify_mode => SSL_VERIFY_NONE,
+        verify_hostname => 0
+    },
+);
+
+die "login failed\n"          unless $pve->login;
+die "invalid login ticket\n"  unless $pve->check_login_ticket;
+die "unsupport api version\n" unless $pve->api_version_check;
+
+# list lxc virtual machines by requesting
+my $resources = $pve->get("/cluster/resources");
+
+for my $item( @$resources ) {
+    next unless $item->{type} eq 'lxc';
+
+    print "id: " .        $item->{id} . "\n";
+    print "cpu: " .       $item->{cpu} . "\n";
+    print "disk: " .      $item->{disk} . "\n";
+    print "maxcpu: " .    $item->{maxcpu} . "\n";
+    print "maxdisk: " .   $item->{maxdisk} . "\n";
+    print "maxmem: " .    $item->{maxmem} . "\n";
+    print "mem: " .       $item->{mem} . "\n";
+    print "node: " .      $item->{node} . "\n";
+    print "type: " .      $item->{type} . "\n";
+    print "uptime: " .    $item->{uptime} . "\n";
+    print "diskread: " .  $item->{diskread} . "\n";
+    print "diskwrite: " . $item->{diskwrite} . "\n";
+    print "name: " .      $item->{name} . "\n";
+    print "netin: " .     $item->{netin} . "\n";
+    print "netout: " .    $item->{netout} . "\n";
+    print "status: " .    $item->{status} . "\n";
+    print "template: " .  $item->{template} . "\n";
+    print "vmid: " .      $item->{vmid} . "\n";
+    print "\n";
+}
